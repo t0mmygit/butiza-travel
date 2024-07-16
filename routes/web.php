3bc6391b7db70bc227aaf25eed139d4d6bf873ae
t0mmygit <tommychinliwei@gmail.com>
2024-07-16 11:27:35 +0800

    Home route fixes

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,12 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-   return Inertia::render('Home'); 
-})->name('home');
+    return redirect('/home');
+});
+
+Route::get('/home', function () {
+    return Inertia::render('Home'); 
+});
 
 Route::get('/about', function () {
     return Inertia::render('About');
