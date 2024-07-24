af14b0cff9f7ad792c2ea9584c8d7305c376181a
t0mmygit <tommychinliwei@gmail.com>
2024-07-24 10:57:57 +0800

    Update: change the partner request file

--- app/Http/Requests/PartnerFormRequest.php
+++ app/Http/Requests/PartnerFormRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class PartnerRequest extends FormRequest
+class PartnerFormRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
