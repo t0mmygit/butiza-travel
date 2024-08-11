6175712a53cbcab060e93c352ab15d9d563c72e2
t0mmygit <tommychinliwei@gmail.com>
2024-08-11 10:24:26 +0800

    Update: Validation for Payment Update.

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Events\BookingPaid;
+use App\Http\Requests\PaymentUpdateRequest;
 use App\Models\Payment;
 use App\Services\PaymentService;
 use Illuminate\Http\RedirectResponse;
@@ -29,25 +30,22 @@ class PaymentController extends Controller
         ]);
     }
 
-    public function update(Request $request, Payment $payment): RedirectResponse
+    public function update(PaymentUpdateRequest $request, Payment $payment): RedirectResponse
     {
         // AUTHORIZATION: Check if user is authorized to update the payment
 
         // TODO: Check if payment is already paid
         // What if they already paid?
         // TODO: Validate payment details
-
+        
         $payment->update([
             'method' => $request->method,
             'status' => 'successful',
         ]);
         
-        // event(new BookingPaid(auth()->user()));
-        // TODO: Event for notification, inbox message, administration notification
-        // [1] Notify Admin
-        // [2] Notify User/Partner
+        $booking = $payment->booking()->get()->first();
 
-        // TODO: Handle email notifications
+        event(new BookingPaid($booking));
 
         return $this->paymentService->redirectAfterPayment();
     }
